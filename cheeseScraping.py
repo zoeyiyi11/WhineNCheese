127b4e1ad0f2727646d6965d1bc35b0a49e8b726
Kayla Branson <kaylabranson@nat-oitwireless-inside-vapornet100-c-26372.Princeton.EDU>
2016-11-12 10:08:47 -0500

    commentz

--- cheeseScraping.py
+++ cheeseScraping.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
 import pyrebase
+# configure and connect with firebase
 config = {
     "apiKey": "AIzaSyCsvb2hDlh-1xJ8V8ZR2DDWdo9HPaS6X80",
     "authDomain": "whine-and-cheese.firebaseapp.com",
@@ -11,27 +12,30 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-soup = BeautifulSoup(open("somecheese.html"),"html.parser")
-text = soup.get_text().lower()#.split()
-text = text.split('\n')
-text = list(filter(None, text)) # fastest
-
 def addWineAndCheese(wine, cheese):
     path = "wines/" + wine
     keys = db.child(path).shallow().get().val()
 
+    # if wine is in database
     if (keys):
         all_cheese = db.child(path).get()
+        # return if cheese is already mapped to wine
         for DBCheese in all_cheese.each():
             if (str(DBCheese.val()).__eq__(cheese)):
                 return
+        # otherwise add cheese with unique numerical key
         key =(len(keys)+1)
         path = "wines/" + wine + "/" + str(key)
         db.child(path).set(cheese)
     else:
+        # add 1 as the first cheese key
         data = {"1": cheese}
         db.child(path).set(data)
 
-
+# actual parsing of cheese file
+soup = BeautifulSoup(open("somecheese.html"),"html.parser")
+text = soup.get_text().lower()
+text = text.split('\n')
+text = list(filter(None, text))
 for i in range(0, (len(text)-1), 2):
     addWineAndCheese(text[i+1], text[i])
