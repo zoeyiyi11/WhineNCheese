a03e790b5155fea6ba8cace4787df56b519a5e98
Kayla Branson <kaylabranson@nat-oitwireless-inside-vapornet100-c-26372.Princeton.EDU>
2016-11-12 12:23:09 -0500

    update database structure

--- cheeseScraping.py
+++ cheeseScraping.py
@@ -12,30 +12,45 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-def addWineAndCheese(wine, cheese):
-    path = "wines/" + wine
-    keys = db.child(path).shallow().get().val()
 
-    # if wine is in database
+def addToDatabaseArray(path, value):
+    keys = db.child(path).shallow().get().val()
+    # if key has a mapped value already, add to the array
     if (keys):
-        all_cheese = db.child(path).get()
-        # return if cheese is already mapped to wine
-        for DBCheese in all_cheese.each():
-            if (str(DBCheese.val()).__eq__(cheese)):
+        all_vals = db.child(path).get()
+        # return if value is already in database
+        for DBCheese in all_vals.each():
+            if (str(DBCheese.val()).__eq__(value)):
                 return
-        # otherwise add cheese with unique numerical key
-        key =(len(keys)+1)
-        path = "wines/" + wine + "/" + str(key)
-        db.child(path).set(cheese)
+        # otherwise add value at next index of array
+        index = len(keys)
+        path = path + "/" + str(index)
+        db.child(path).set(value)
     else:
-        # add 1 as the first cheese key
-        data = {"1": cheese}
+        # add first element of array (index 0)
+        data = {"0":value}
         db.child(path).set(data)
 
-# actual parsing of cheese file
-soup = BeautifulSoup(open("somecheese.html"),"html.parser")
-text = soup.get_text().lower()
-text = text.split('\n')
-text = list(filter(None, text))
-for i in range(0, (len(text)-1), 2):
-    addWineAndCheese(text[i+1], text[i])
+def addCheesyResponse(soCheesy):
+    path = "cheesyResponses"
+    keys = db.child(path).shallow().get().val()
+    index = len(keys)
+    path = path + "/" + str(index)
+    db.child(path).set(soCheesy)
+
+def addWineAndCheese(wine, cheese):
+    path = "wines/" + wine + "/cheeses"
+    addToDatabaseArray(path, cheese)
+    path = "wines/" + wine + "/type"
+    wine_type = db.child(path).shallow().get().val()
+    if (not wine_type):
+        db.child(path).set("")
+
+
+# # actual parsing of cheese file
+# soup = BeautifulSoup(open("somecheese.html"),"html.parser")
+# text = soup.get_text().lower()
+# text = text.split('\n')
+# text = list(filter(None, text))
+# for i in range(0, (len(text)-1), 2):
+#     addWineAndCheese(text[i+1], text[i])
